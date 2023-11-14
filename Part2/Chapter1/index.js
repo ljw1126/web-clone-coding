af4a32f78b9e3b89c8f89532d6503f3f9d2ea3b1
leejinwoo <leejinwoo1126@gmail.com>
2023-11-14 15:26:24 +0900

    feat: 1-3 파티클 그리기(클래스 정의, requestAnimationFrame)

--- Part2/Chapter1/index.js
+++ Part2/Chapter1/index.js
@@ -16,4 +16,36 @@ canvas.width = canvasWidth * dpr;
 canvas.height = canvasHeight * dpr;
 ctx.scale(dpr, dpr);
 
-ctx.fillRect(10, 10, 50, 50);
+
+
+class Particle {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI / 180 * 360); // 시작 반지름, 시작 각도, 종료 각도
+    ctx.fillStyle = "red"
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+const x = 100;
+const y = 100;
+const radius = 50;
+const particle = new Particle(x, y, radius);
+
+
+
+function animate() {
+  window.requestAnimationFrame(animate); // 매 프레임마다 실행됨 
+  
+  // 전체 화면을 매 프레임마다 지우고, particle을 그림
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight); 
+  particle.draw();
+}
+
+animate();

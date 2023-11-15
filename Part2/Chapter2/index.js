d176aa2ea35df3bab1a63c4d62386d6c8d2c48cf
leejinwoo <leejinwoo1126@gmail.com>
2023-11-15 21:50:45 +0900

    feat: 2-4 불꽃놀이 원 모양으로 랜덤 파티클 만들기

--- Part2/Chapter2/index.js
+++ Part2/Chapter2/index.js
@@ -1,6 +1,6 @@
 import CanvasOption from './js/CanvasOption.js';
 import Particle from './js/Particle.js';
-import { randomNumBetween } from './js/utils.js';
+import { randomNumBetween, hypotenuse } from './js/utils.js';
 
 class Canvas extends CanvasOption {
   constructor() {
@@ -22,13 +22,18 @@ class Canvas extends CanvasOption {
   }
 
   createParticles() {
-    const PARTICLE_NUM = 2000;
+    const PARTICLE_NUM = 200;
     const x = randomNumBetween(0, this.canvasWidth);
     const y = randomNumBetween(0, this.canvasHeight);
     for(let i = 0; i < PARTICLE_NUM; i++) {
-      const vx= randomNumBetween(-5, 5);
-      const vy = randomNumBetween(-5, 5);
-      this.particles.push(new Particle(x, y, vx, vy));
+      const r = randomNumBetween(2, 100) * hypotenuse(innerWidth, innerHeight) * 0.0001;
+      const angle = Math.PI / 180 * randomNumBetween(0, 360);
+
+      const vx= r * Math.cos(angle);
+      const vy = r * Math.sin(angle);
+
+      const opacity= randomNumBetween(0.6, 0.9);
+      this.particles.push(new Particle(x, y, vx, vy, opacity));
     }
   }
 
@@ -43,7 +48,7 @@ class Canvas extends CanvasOption {
       
       if(delta < this.interval) return;
       
-      this.ctx.fillStyle = this.bgColor;
+      this.ctx.fillStyle = this.bgColor + '40'; // #00000010 rgba 알파값 추가되어 contrast, blur 효과줌
       this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
 
       this.particles.forEach((particle, index) => {

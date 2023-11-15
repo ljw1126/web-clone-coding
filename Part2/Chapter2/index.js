8e6514637847ed7afa069a3c799237d78441227b
leejinwoo <leejinwoo1126@gmail.com>
2023-11-15 22:44:23 +0900

    feat: 2-6. 스파크 작업과 기타 꾸미기

--- Part2/Chapter2/index.js
+++ Part2/Chapter2/index.js
@@ -2,6 +2,7 @@ import CanvasOption from './js/CanvasOption.js';
 import Particle from './js/Particle.js';
 import { randomNumBetween, hypotenuse } from './js/utils.js';
 import Tail from './js/tail.js';
+import Spark from './js/Spark.js';
 
 class Canvas extends CanvasOption {
   constructor() {
@@ -9,6 +10,7 @@ class Canvas extends CanvasOption {
 
     this.particles = [];
     this.tails = [];
+    this.sparks = [];
   }
   init() {
     this.canvasWidth = innerWidth;
@@ -25,11 +27,11 @@ class Canvas extends CanvasOption {
   createTails() {
     const x = randomNumBetween(this.canvasWidth * 0.2, this.canvasWidth * 0.8); // 20 ~ 80% 위치
     const vy = this.canvasHeight * randomNumBetween(0.01, 0.015) * -1;
-    const color = '255, 255, 255';
-    this.tails.push(new Tail(x, vy, color));
+    const colorDeg = randomNumBetween(0, 360); 
+    this.tails.push(new Tail(x, vy, colorDeg));
   }
 
-  createParticles(x, y, color) {
+  createParticles(x, y, colorDeg) {
     const PARTICLE_NUM = 200;
     for(let i = 0; i < PARTICLE_NUM; i++) {
       const r = randomNumBetween(2, 100) * hypotenuse(innerWidth, innerHeight) * 0.0001;
@@ -39,7 +41,8 @@ class Canvas extends CanvasOption {
       const vy = r * Math.sin(angle);
 
       const opacity= randomNumBetween(0.6, 0.9);
-      this.particles.push(new Particle(x, y, vx, vy, opacity, color));
+      const _colorDeg = randomNumBetween(-20, 20) + colorDeg;
+      this.particles.push(new Particle(x, y, vx, vy, opacity, _colorDeg));
     }
   }
 
@@ -57,11 +60,22 @@ class Canvas extends CanvasOption {
       this.ctx.fillStyle = this.bgColor + '40'; // #00000010 rgba 알파값 추가되어 contrast, blur 효과줌
       this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
 
+      this.ctx.fillStyle = `rgba(255, 255, 255, ${this.particles.length / 20000})`;
+      this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+
       if(Math.random() < 0.03) this.createTails();
 
       this.tails.forEach((tail, idx) => {
         tail.update();
         tail.draw();
+
+        // TODO. 이해 안됨
+        for(let i = 0; i < Math.round( -tail.vy * 0.5); i++) {
+          const vx = randomNumBetween(-5, 5) * 0.05;
+          const vy = randomNumBetween(-5, 5) * 0.05;
+          const opacity = Math.min(-tail.vy, 0.5);
+          this.sparks.push(new Spark(tail.x, tail.y, vx, vy, opacity, tail.olorDeg));
+        }
 
         if(tail.vy > -0.7) { // vy는 항상 minus 값이니
           this.tails.splice(idx, 1);
@@ -73,11 +87,21 @@ class Canvas extends CanvasOption {
         particle.update();
         particle.draw();
         
+        if(Math.random() < 0.1) {
+          this.sparks.push(new Spark(particle.x, particle.y, 0, 0, 0.3));
+        }
+        
         if(particle.opacity < 0) {
             this.particles.splice(index, 1);
         }
       });
 
+      this.sparks.forEach((spark, idx) => {
+        spark.update();
+        spark.draw();
+
+        if(spark.opacity < 0) this.sparks.splice(idx, 1);
+      });
 
 
       then = now - (delta % this.interval);
--- Part2/Chapter2/js/tail.js
+++ Part2/Chapter2/js/tail.js
@@ -2,12 +2,12 @@ import CanvasOption from './CanvasOption.js';
 import { randomNumBetween } from './utils.js';
 
 export default class Tail extends CanvasOption {
-  constructor(x, vy, color) {
+  constructor(x, vy, colorDeg) {
     super();
     this.x = x;
     this.y = this.canvasHeight;
     this.vy = vy;
-    this.color = color;
+    this.colorDeg = colorDeg;
     this.friction = 0.985; // 마찰
     this.angle = randomNumBetween(0, 2); // 꼬리 움직임
   }
@@ -21,7 +21,7 @@ export default class Tail extends CanvasOption {
     this.opacity = -this.vy * 0.1; // this.vy는 항상 minus 값이니 0.1 곱해서 0에 수렴
   }
   draw() {
-    this.ctx.fillStyle = `rgba(${this.color}, ${this.opacity})`;
+    this.ctx.fillStyle = `hsla(${this.colorDeg}, 100%, 65%, ${this.opacity})`;
     this.ctx.beginPath();
     this.ctx.arc(this.x, this.y, 1, 0, Math.PI *2); // 원
     this.ctx.fill();

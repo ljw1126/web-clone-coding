104d1a4801ea86088336983e8e869cb9721b2ce1
leejinwoo <leejinwoo1126@gmail.com>
2023-11-26 22:31:42 +0900

    feat: 2-12. 다면체로 변경하기

--- Part3/1-cube/src/main.js
+++ Part3/1-cube/src/main.js
@@ -26,33 +26,33 @@ function init() {
   )
 
   // 4. 3d object 추가
-  const geometry = new THREE.BoxGeometry(2, 2, 2); // 너비, 높이, 깊이
-  const material = new THREE.MeshStandardMaterial({
+  const cubeGeometry = new THREE.IcosahedronGeometry(1); // 너비, 높이, 깊이
+  const cubeMaterial = new THREE.MeshLambertMaterial({
     color: 0xcc99ff,
-    transparent: true, // 불투명도 조절위한 설정 활성화, opacity로 조절
-    opacity: 1,
-    //visible : false,
-    //wireframe: true, // material 뼈대 확인용
-    //side: THREE.DoubleSide // 기본 FrontSide, 이외 BackSide, DoubleSide있음(부하 증가)
+    emissive: 0x111111,
   });
+
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
   
-  //material.color = new THREE.Color(0xf0f0f0);
+  const skeletonGemometry = new THREE.IcosahedronGeometry(2);
+  const skeletonMaterial = new THREE.MeshBasicMaterial({
+    wireframe: true,
+    transparent: true,
+    opacity: 0.3,
+    color: 0xaaaaaa
+  });
 
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  const skeleton = new THREE.Mesh(skeletonGemometry, skeletonMaterial);
 
-  camera.position.set(3, 4, 5); // x, y, z
 
-  camera.lookAt(cube.position);
+  scene.add(cube, skeleton);
 
+  camera.position.z = 5;
+  
   // 5. 조명 추가 
-  const directionalLight = new THREE.DirectionalLight(0xf0f0f0, 1);
-  directionalLight.position.set(-1, 2, 3);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
   scene.add(directionalLight);
-
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.1); // mesh 우측 영역까지 은은하게 보이도록 조명 추가
-  ambientLight.position.set(3, 2, 1);
-  scene.add(ambientLight);
 
   // 애니메이션 효과 적용
   const clock = new THREE.Clock();
@@ -60,11 +60,13 @@ function init() {
   render();
 
   function render() {
-    //cube.rotation.x = THREE.MathUtils.degToRad(45); // 라디안 단위
-    //cube.rotation.x = Date.now() / 1000;
-    cube.rotation.x = clock.getElapsedTime(); // delta는 더하는 형태로 사용
-    // cube.position.y = Math.sin(cube.rotation.x); // 위 아래 움직임
-    // cube.scale.x = Math.cos(cube.rotation.x); // 좌우 방향 
+    const elapsedTime = clock.getElapsedTime();
+
+    cube.rotation.x = elapsedTime;
+    cube.rotation.y = elapsedTime;
+
+    skeleton.rotation.x = elapsedTime * 0.5;
+    skeleton.rotation.y = elapsedTime * 0.5;
 
     renderer.render(scene, camera);
 

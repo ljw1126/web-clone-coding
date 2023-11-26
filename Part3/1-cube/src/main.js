27c7c665243772411120276121741a498aa50a1a
leejinwoo <leejinwoo1126@gmail.com>
2023-11-26 22:49:50 +0900

    feat: 2-14 GUI 컨트롤러 추가(lil-gui)

--- Part3/1-cube/src/main.js
+++ Part3/1-cube/src/main.js
@@ -1,10 +1,14 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import GUI from 'lil-gui'
 
 window.addEventListener("load", () => {
   init();
 });
 
 function init() {
+
+
   // 1. 캔버스 가짐
   const renderer = new THREE.WebGL1Renderer({
     antialias: true // 큐브 면에 까끌한거 제거
@@ -24,6 +28,24 @@ function init() {
     1,
     500
   )
+
+
+  // OribitControls
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.autoRotate = true;
+  controls.enableDamping = true; // 관성
+  // controls.dampingFactor = 0.01; 
+  // controls.enableZoom = true; // 카메라 줌 확대/축소 활성
+  // controls.enablePan = true;
+  // controls.maxDistance = 50; // 확대 축소 최대 최소
+  // controls.minDistance = 10;
+  controls.maxPolarAngle = Math.PI / 2; // 수직, 수평 돌아가는 각도도 제어 가능
+  controls.minPolarAngle = Math.PI / 3;
+
+  // const axesHelper = new THREE.AxesHelper(5);
+  // scene.add(axesHelper);
+
+
 
   // 4. 3d object 추가
   const cubeGeometry = new THREE.IcosahedronGeometry(1); // 너비, 높이, 깊이
@@ -62,13 +84,15 @@ function init() {
   function render() {
     const elapsedTime = clock.getElapsedTime();
 
-    cube.rotation.x = elapsedTime;
-    cube.rotation.y = elapsedTime;
+    // cube.rotation.x = elapsedTime;
+    // cube.rotation.y = elapsedTime;
 
-    skeleton.rotation.x = elapsedTime * 0.5;
-    skeleton.rotation.y = elapsedTime * 0.5;
+    // skeleton.rotation.x = elapsedTime * 0.5;
+    // skeleton.rotation.y = elapsedTime * 0.5;
 
     renderer.render(scene, camera);
+
+    controls.update();
 
     requestAnimationFrame(render);
   }
@@ -80,7 +104,26 @@ function init() {
 
     renderer.setSize(innerWidth, innerHeight);
     renderer.render(scene, camera);
+
+    controls.update();
   }
 
   window.addEventListener("resize", handleResize);
+
+  // GUI
+  const gui = new GUI();
+  //gui.add(cube.position, 'y', -3, 3, 0.1);
+  gui
+    .add(cube.position, 'y')
+    .min(-3)
+    .max(3)
+    .step(0.1);
+
+  gui.add(cube, 'visible');
+
+  const options = { color: 0x00ffff }
+  gui.addColor(options, 'color')
+    .onChange((value) => {
+      cube.material.color.set(value);
+    });
 }

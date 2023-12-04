52f4aac41a208d1e62f860402011a5aa9d447df4
leejinwoo <leejinwoo1126@gmail.com>
2023-12-04 11:02:56 +0900

    feat: ch4-5. StandardMaterial을 사용하여 카드 재질 표현

--- Part3/3-interactive-card/src/Card.js
+++ Part3/3-interactive-card/src/Card.js
@@ -5,7 +5,9 @@ export default class Card {
     const geometry = new THREE.PlaneGeometry(width, height);
     const material = new THREE.MeshStandardMaterial({
       color,
-      side: THREE.DoubleSide 
+      side: THREE.DoubleSide ,
+      roughness: 0.5,
+      metalness: 0.5
     }); // 조명 설정해줘야 색상 반영됨
 
     const mesh = new THREE.Mesh(geometry, material);
--- Part3/3-interactive-card/src/main.js
+++ Part3/3-interactive-card/src/main.js
@@ -1,12 +1,15 @@
 import * as THREE from 'three';
 import Card from './Card.js';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import {GUI} from 'lil-gui';
 
 window.addEventListener('load', function () {
   init();
 });
 
 function init() {
+  const gui = new GUI();
+
   const renderer = new THREE.WebGLRenderer({
     antialias: true,
     alpha: true
@@ -37,10 +40,34 @@ function init() {
 
   scene.add(card.mesh);
 
+  // gui 
+  const cardFolder = gui.addFolder("Card");
+  
+  cardFolder 
+  .add(card.mesh.material, "roughness")
+  .min(0)
+  .max(1)
+  .step(0.01)
+  .name("material.roughness");
+
+  cardFolder 
+  .add(card.mesh.material, "metalness")
+  .min(0)
+  .max(1)
+  .step(0.01)
+  .name("material.metalness");
+
   // 조명 
   const ambientLight = new THREE.AmbientLight(0xffffff, 2);
   ambientLight.position.set(-5, -5, -5);
   scene.add(ambientLight);
+
+  const directionalLight1 = new THREE.DirectionalLight(0xffffff, 0.6);
+  const directionalLight2 = directionalLight1.clone();
+
+  directionalLight1.position.set(1, 1, 3);
+  directionalLight2.position.set(-1, 1, -3);
+  scene.add(directionalLight1, directionalLight2);
 
   render();
 

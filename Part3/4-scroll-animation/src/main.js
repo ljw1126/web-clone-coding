401f61ea1df1e364af9e2670de6378c5019e74bb
leejinwoo <leejinwoo1126@gmail.com>
2023-12-04 12:15:23 +0900

    feat: ch5-4. 파도 추가2

--- Part3/4-scroll-animation/src/main.js
+++ Part3/4-scroll-animation/src/main.js
@@ -29,17 +29,26 @@ function init() {
 
   const waveGeometry = new THREE.PlaneGeometry(1500, 1500, 150, 150);
   const waveMaterial = new THREE.MeshStandardMaterial({
-    wireframe: true,
     color: '#00ffff',
   });
+
+  //console.log(waveGeometry.attributes.position);
   
   const wave = new THREE.Mesh(waveGeometry, waveMaterial);
   wave.rotation.x = -Math.PI / 2;
 
+  const waveHeight = 2.5;
+
+  for(let i = 0; i < waveGeometry.attributes.position.count; i++) {
+    const z = waveGeometry.attributes.position.getZ(i) + (Math.random() - 0.5) * waveHeight;
+
+    waveGeometry.attributes.position.setZ(i, z);
+  }  
+
   scene.add(wave); 
   
   // 조명 
-  const pointLight = new THREE.PointLight(0xffffff, 1);
+  const pointLight = new THREE.PointLight(0xffffff, 4000, 0);
   pointLight.position.set(15, 15, 15);
   scene.add(pointLight);
 
